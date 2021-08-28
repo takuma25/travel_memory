421479901fe6c395d4824c73b3a041e210b46de8
takuma25 <t.o12.13@icloud.com>
2021-08-28 12:23:21 +0000

    [Fix] Migration

--- db/migrate/20210816111747_rename_category_name_column_to_categories.rb
+++ db/migrate/20210816111747_rename_category_name_column_to_categories.rb
@@ -1,6 +1,6 @@
 class RenameCategoryNameColumnToCategories < ActiveRecord::Migration[5.2]
   def change
     rename_column :categories, :category_name, :name
-    rename_column :categories, :detail_name, :ancestry
+    # rename_column :categories, :detail_name, :ancestry
   end
 end

0e7195caf710a62b82b7bdc4ab2035b46ea097d0
takuma25 <t.o12.13@icloud.com>
2021-08-28 04:15:29 +0000

    test2 Commit

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,8 +1,8 @@
 class HomesController < ApplicationController
   
   def top
-    @article = Article.limit(10).order(" created_at DESC ")
-    @all_ranks = Article.find(Like.group(:article_id).order('count(article_id) desc').limit(3).pluck(:article_id))
+    @article = Article.limit(5).order(" created_at DESC ")
+    @all_ranks = Article.find(Like.group(:article_id).order('count(article_id) desc').limit(5).pluck(:article_id))
   end
   
 end
